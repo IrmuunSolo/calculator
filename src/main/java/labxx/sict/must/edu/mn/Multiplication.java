4692fbbdea5f335952b5ba9400e7d8c755c25f44
IrmuunSolo <Oyutan6677gmail.com>
2025-05-09 19:55:45 +0800

    Үржүүлэх функцийн Javadoc нэмсэн

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -1,6 +1,17 @@
-package main.java.labxx.sict.must.edu.mn;
+package labxx.sict.must.edu.mn;
 
+/**
+ * Хоёр тооны үржвэрийг олдог класс
+ */
 public class Multiplication {
+    /**
+     * Хоёр тоог үржүүлэх функц
+     *
+     * @param a эхний үржүүлэгч (дурын бодит тоо)
+     * @param b хоёр дахь үржүүлэгч (дурын бодит тоо)
+     * @return a ба b-ийн үржвэр (a × b)
+     * @throws ArithmeticException хэрэв үржүүлэлт тоон хэлбэрээр илэрхийлэгдэхгүй бол
+     */
     public double multiply(double a, double b) {
         return a * b;
     }
